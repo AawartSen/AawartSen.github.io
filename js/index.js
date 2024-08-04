41c466b56e1ede137f35591cb6be9d280beaff03
Aawart Sen <surendra.katuwal331@gmail.com>
2024-08-04 16:39:58 +0545

    refactor

--- js/index.js
+++ js/index.js
@@ -1,110 +1,159 @@
-let score = 0;
-let currentProblem = null;
+const operations = ["+", "-", "*", "/"];
+const NotificationType = {
+  CORRECT: "correct",
+  INCORRECT: "incorrect",
+};
+const AnimationState = {
+  HAPPY: "happy",
+  SAD: "sad",
+};
 
-document.getElementById("userAnswer").addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        checkAnswer();
-    }
-});
+var score = 0;
+var currentProblem = randomProblem();
+var currentQuestion = `What is ${currentProblem.question}?`;
+var integerAnswer = currentProblem.answer;
+
+/////////////////////////////////////////////////////////////////////////////////
 
 displayQuestion();
 
+document
+  .getElementById("userAnswer")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      checkAnswer();
+    }
+  });
+
+/////////////////////////////////////////////////////////////////////////////////
+
 function displayQuestion() {
-    currentProblem = randomProblem();
-    document.getElementById("question").textContent = `What is ${currentProblem.question}?`;
-    document.getElementById("userAnswer").value = "";
-    document.getElementById("answer").textContent = "";
+  document.getElementById("question").textContent = currentQuestion;
+  document.getElementById("userAnswer").value = "";
+  document.getElementById("answer").textContent = "";
 }
 
 function randomProblem() {
-    let num1 = Math.floor(Math.random() * 10) + 1;
-    let num2 = Math.floor(Math.random() * 10) + 1;
-    let operation = Math.floor(Math.random() * 4);
+  let num1 = Math.floor(Math.random() * 10) + 1;
+  let num2 = Math.floor(Math.random() * 10) + 1;
+  let operation = operations[Math.floor(Math.random() * operations.length)];
 
-    let qsn;
-    let ans;
+  let ans = 0;
+  if (operation === "+") {
+    ans = num1 + num2;
+  } else if (operation === "-") {
+    ans = num1 - num2;
+  } else if (operation === "*") {
+    ans = num1 * num2;
+  } else if (operation === "/") {
+    ans = num1 / num2;
+  }
 
-    if (operation === 0) {
-        qsn = `${num1} + ${num2}`;
-        ans = num1 + num2;
-    } else if (operation === 1) {
-        qsn = `${num1} - ${num2}`;
-        ans = num1 - num2;
-    } else if (operation === 2) {
-        qsn = `${num1} * ${num2}`;
-        ans = num1 * num2;
-    } else if (operation === 3) {
-        qsn = `${num1} / ${num2}`;
-        ans = num1 / num2;
-    }
-
-    return { question: qsn, answer: ans };
+  return { question: `${num1} ${operation} ${num2}`, answer: parseInt(ans) };
 }
 
+/////////////////////////////////////////////////////////////////////////////////
 
 function checkAnswer() {
-    const userAnswerElement = document.getElementById("userAnswer");
-    const userAnswer = parseFloat(userAnswerElement.value);
-    const correctAnswer = currentProblem.answer;
-    
-    if (isNaN(userAnswer)) {
-        alert("Please enter a valid number.");
-        return;
-    }
-    
-    if (userAnswer === parseInt(correctAnswer)) {
-        score++;
-        document.getElementById("score").textContent = `Score: ${score}`;
-        showNotification("Correct Answer!", "correct");
-        
-        const icons = document.querySelectorAll("footer .icon");
-        applyWaveAnimation(icons);
-        setTimeout(() => {
-            applyWaveAnimation(icons);
-        }, 500);
-        
-        setTimeout(() => {
-            displayQuestion();
-        }, 1000);
-        
-    } else {
-        showNotification(`Incorrect Answer! The correct answer was ${correctAnswer}.`, "incorrect", () => {
-            userAnswerElement.disabled = true;
-            document.querySelector("button").disabled = true;
-            
-            setTimeout(() => {
-                userAnswerElement.disabled = false;
-                document.querySelector("button").disabled = false;
-                displayQuestion();
-            }, 200);
-        });
-    }
-}
+  var userAnswer = parseInt(document.getElementById("userAnswer").value);
 
-function showNotification(message, type, callback) {
-    const notification = document.getElementById("notification");
-    notification.textContent = message;
-    notification.className = `show ${type}`;
+  if (isNaN(userAnswer)) {
+    alert("Please enter a valid number.");
+    return;
+  }
 
-    if (notification.timeout) {
-        clearTimeout(notification.timeout);
+  if (userAnswer === integerAnswer) {
+    showNotification("Correct Answer!", NotificationType.CORRECT);
+
+    for (let i = 0; i < 2; i++) {
+      setTimeout(() => {
+        applyAnimation(AnimationState.HAPPY);
+      }, i * 500);
     }
 
-    notification.timeout = setTimeout(() => {
-        notification.classList.remove("show");
-        if (callback) callback();
+    setTimeout(() => {
+      displayQuestion();
     }, 1000);
+
+    score++;
+    document.getElementById("score").textContent = "Score: " + score;
+  } else {
+    document.getElementById("userAnswer").disabled = true;
+    document.querySelector("button").disabled = true;
+
+    showNotification(
+      "The correct answer was " + integerAnswer + ".",
+      NotificationType.INCORRECT
+    );
+    applyAnimation(AnimationState.SAD);
+
+    setTimeout(() => {
+      document.getElementById("userAnswer").disabled = false;
+      document.querySelector("button").disabled = false;
+      document.getElementById("userAnswer").focus();
+    }, 1000);
+
+    setTimeout(() => {
+      score = 0;
+      document.getElementById("score").textContent = "Score: " + score;
+    }, 1000);
+  }
+
+  setTimeout(() => {
+    currentProblem = randomProblem();
+    currentQuestion = `What is ${currentProblem.question}?`;
+    integerAnswer = currentProblem.answer;
+    displayQuestion();
+  }, 1000);
 }
 
-function applyWaveAnimation(elements) {
-    elements.forEach((icon) => {
-        icon.classList.remove("wave-animation");
-        void icon.offsetWidth;
-        icon.classList.add("wave-animation");
+/////////////////////////////////////////////////////////////////////////////////
 
-        icon.addEventListener("animationend", () => {
-            icon.classList.remove("wave-animation");
-        }, { once: true });
-    });
+function showNotification(message, type) {
+  var notification = document.getElementById("notification");
+  notification.textContent = message;
+  notification.className = "show " + type;
+
+  if (notification.timeout) {
+    clearTimeout(notification.timeout);
+  }
+
+  notification.timeout = setTimeout(() => {
+    notification.classList.remove("show");
+  }, 1000);
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+function applyAnimation(state) {
+  const footer = document.getElementsByTagName("footer")[0];
+  const resetAnimation = () => {
+    footer.classList.remove("happy-animation", "sad-animation");
+    void footer.offsetWidth;
+  };
+
+  if (state === AnimationState.HAPPY) {
+    resetAnimation();
+    footer.classList.add("happy-animation");
+    footer.addEventListener(
+      "animationend",
+      () => {
+        footer.classList.remove("happy-animation");
+      },
+      { once: true }
+    );
+  } else if (state === AnimationState.SAD) {
+    resetAnimation();
+    footer.classList.add("sad-animation");
+    footer.addEventListener(
+      "animationend",
+      () => {
+        footer.classList.remove("sad-animation");
+      },
+      { once: true }
+    );
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
